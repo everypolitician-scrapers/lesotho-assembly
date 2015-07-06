09adeee0c9fa92f2281b3fc5e239a3548c5a6cfc
Tony Bowden <tony@tmtm.com>
2015-07-06 21:44:41 +0100

    Cross-match the data with the Electoral Commission data
    This gives us party information for the constituency representatives
    (and gender)

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,9 @@
 require 'scraperwiki'
 require 'nokogiri'
 require 'open-uri'
+require 'cgi'
+require 'json'
+require 'fuzzy_match'
 
 require 'pry'
 require 'open-uri/cached'
@@ -13,6 +16,20 @@ def noko_for(url)
   Nokogiri::HTML(open(url).read) 
 end
 
+def candidates
+  data = open("https://api.morph.io/tmtmtmtm/lesotho-electoral-commission/data.json?query=select%20*%20from%20data&key=" + CGI.escape(ENV["MORPH_API_KEY"])).read
+  JSON.parse(data, symbolize_names: true)
+end
+
+def find_candidate(data)
+  matches = @candidates.find_all { |c| c[:constituency_id] == data[:area_id].to_i }.each do |c|
+    c[:name] = "#{c[:given_name]} #{c[:family_name]}"
+  end
+  match = FuzzyMatch.new(matches.map { |m| m[:name] }).find(data[:name]) or return
+  return matches.find { |m| m[:name] == match }
+end
+
+
 def scrape_list(url)
   noko = noko_for(url)
   noko.css('#ja-content table tr').drop(1).each do |row|
@@ -20,11 +37,17 @@ def scrape_list(url)
     data = { 
       name: tds[0].text.sub('Mohl. ','').strip,
       area: tds[1].text.strip,
+      area_id: tds[2].text.strip,
       party: tds[3].text.strip,
       term: 9,
       source: url,
     }
     next if data[:name].empty?
+    if data[:party].to_s.empty? and candidate = find_candidate(data) 
+      data[:party] = candidate[:party]
+      data[:gender] = candidate[:gender]
+      data[:age] = candidate[:age]
+    end
     puts data
     ScraperWiki.save_sqlite([:name, :term], data)
   end
@@ -38,4 +61,5 @@ term = {
 }
 ScraperWiki.save_sqlite([:id], term, 'terms')
 
+@candidates = candidates
 scrape_list('http://www.parliament.ls/assembly/index.php?option=com_content&view=article&id=37&Itemid=56')
